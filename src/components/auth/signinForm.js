674ee08b417c12e44183e655aaa9f45d45b72f07
edanrenecreates <rene@edanramos.com>
2020-09-27 20:34:28 -0600

    styled form fields

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -8,8 +8,21 @@ class SigninForm extends Component {
   render() {
     return (
       <form className='sign-in-form'>
-        <FormTitle className='sign0in__title' text='Login'/>
-        <Field className='sign-in-form__email' component={FormInput} name='email' type='email'/>
+        <FormTitle className='sign-in-form__title' text='Login' />
+        <Field className='sign-in-form__email'
+          placeholder='Enter email'
+          name='email'
+          type='email'
+          title="Email"
+          component={FormInput}
+        />
+        <Field className='sign-in-form__password'
+          placeholder='Enter password'
+          name='password'
+          type='password'
+          title="Password"
+          component={FormInput}
+        />
       </form>
     )
   }
