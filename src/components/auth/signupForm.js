50349224509aca431c68b6fad807850421090f7c
edanrenecreates <rene@edanramos.com>
2020-09-28 18:08:44 -0600

    built signup form

---
+++ src/components/auth/signupForm.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+
+import { FormTitle } from '../formTitle';
+import { FormInput, FormButton } from '../formFields';
+import TextLink from '../textLink';
+
+class SignupForm extends Component {
+  render() {
+    return (
+      <form className='sign-up-form'>     
+        <FormTitle className='sign-up-form__title' text='New User' />
+
+        <Field className='sign-up-fullName'
+          placeholder='Enter Your Full Name'
+          name='fullname'
+          type='text'
+          title="Full Name"
+          component={FormInput}
+        />
+
+        <Field className='sign-up-form__unit'
+          placeholder='Enter Unit Number'
+          name='unit'
+          type='text'
+          title="Unit #"
+          component={FormInput}
+        />
+        <Field className='sign-up-form__email'
+          placeholder='Enter email'
+          name='email'
+          type='email'
+          title="Email"
+          component={FormInput}
+        />
+        <Field className='sign-up-form__password'
+          placeholder='Enter password'
+          name='password'
+          type='password'
+          title="Password"
+          component={FormInput}
+        />
+        <Field className='sign-up-form__create-account'
+          name='createaccount'
+          type='Submit'
+          title="Create Account"
+          component={FormButton}
+        />
+        <div className='sign-up-form__text-links'>
+          <TextLink to='/signup/' text='Already Registered? Login' />
+        </div>
+      </form>
+    )
+  }
+}
+
+SignupForm = reduxForm({
+  form: 'signup'
+})(SignupForm);
+
+export default SignupForm;
