f8a90fea5f31d210ee89989b69931bdbfedf8792
edanrenecreates <rene@edanramos.com>
2020-09-29 20:05:51 -0600

    finished require auth and created browser history manually

--- src/components/requireAuth.js
+++ src/components/requireAuth.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import history from '../history';
+
 export default function(ComposeComponent) {
   class Authentication extends Component {
     componentWillMount() {
       if(!this.props.authenticated) {
-        this.props.history.push('/')
+        history.push('/');
       }
     }
-    componentWillUpdate() {
-
+    componentWillUpdate(nextProps) {
+      if(!nextProps.authenticated) {
+        history.push('/');
+      }
     }
     render() {
       return <ComposeComponent/>
@@ -17,8 +21,8 @@ export default function(ComposeComponent) {
   }
 
   function mapStateToProps(state) {
-    const { authenticated } - state.auth;
-    return { authenticated } ;
+    const { authenticated } = state.auth;
+    return { authenticated };
   }
 
   return connect(mapStateToProps)(Authentication)
