1b3743809b0e7fc5e91e4892095b599c6b6a1e1a
edanrenecreates <rene@edanramos.com>
2020-10-05 16:01:59 -0600

    mapped over a newsletter JSON item to the latest newsletter component

--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -6,12 +6,20 @@ import NewsletterLatest from './newsletterLatest';
 
 class NewsletterGrid extends Component {
   render() {
+
+    const latest = {
+      __id: '115',
+      title: 'Happy Holidays!',
+      body: 'Aute man braid aliquip, tempor bushwick ipsum synth whatever biodiesel adaptogen keytar. Air plant PBRB subway tile chicharrones gentrify iceland post-ironic. Whatever church-key velit locavore deserunt sunt pok pok. Bespoke trust fund locavore yuccie lomo messenger bag. Man braid yuccie quis man bun veniam glossier reprehenderit meh vaporware ex af butcher in forage aesthetic. Keytar disrupt lo-fi scenester ex pork belly crucifix raw denim deep v.',
+      date: new Date(),
+      imageUrl: 'http://via.placeholder.com/960x258'
+    }
     return(
       <div className='newsletter-grid'>
         {/* Add button */}
         <NewsletterBox date={new Date()}/>
         <NewsletterArchive/>
-        <NewsletterLatest/>
+        <NewsletterLatest {...latest}/>
       </div>
     )
   }
--- src/components/newsletter/newsletterLatest.js
+++ src/components/newsletter/newsletterLatest.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 
 class NewsletterLatest extends Component {
   render() {
-    return (
+    const { title, imageUrl, body} = this.props;
+   return (
       <div className='newsletter-latest'>
-        <h1 className='newsletter-latest__title'>Title goes here</h1>
-        <img className='newsletter-latest__image' src='http://via.placeholder.com/960x258'/>
+        <h1 className='newsletter-latest__title'>{title}</h1>
+        <img className='newsletter-latest__image' src={imageUrl}/>
         <div className='newsletter-latest__body'>
-          <p>Aute man braid aliquip, tempor bushwick ipsum synth whatever biodiesel adaptogen keytar. Air plant PBR&B subway tile chicharrones gentrify iceland post-ironic. Whatever church-key velit locavore deserunt sunt pok pok. Bespoke trust fund locavore yuccie lomo messenger bag. Man braid yuccie quis man bun veniam glossier reprehenderit meh vaporware ex af butcher in forage aesthetic. Keytar disrupt lo-fi scenester ex pork belly crucifix raw denim deep v.</p>
+        <p>{body}</p>
         </div>
       </div>
        
