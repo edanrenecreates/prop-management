9fa2e624be3e3ed99082f70521fbf13a50f915cf
edanrenecreates <rene@edanramos.com>
2020-10-05 15:48:08 -0600

    styled newsletter latest and applied row gaps to the newsletter grid

---
+++ src/components/newsletter/newsletterLatest.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class NewsletterLatest extends Component {
+  render() {
+    return (
+      <div className='newsletter-latest'>
+        <h1 className='newsletter-latest__title'>Title goes here</h1>
+        <img className='newsletter-latest__image' src='http://via.placeholder.com/960x258'/>
+        <div className='newsletter-latest__body'>
+          <p>Aute man braid aliquip, tempor bushwick ipsum synth whatever biodiesel adaptogen keytar. Air plant PBR&B subway tile chicharrones gentrify iceland post-ironic. Whatever church-key velit locavore deserunt sunt pok pok. Bespoke trust fund locavore yuccie lomo messenger bag. Man braid yuccie quis man bun veniam glossier reprehenderit meh vaporware ex af butcher in forage aesthetic. Keytar disrupt lo-fi scenester ex pork belly crucifix raw denim deep v.</p>
+        </div>
+      </div>
+       
+    );
+  }
+}
+
+
+export default NewsletterLatest;
