8ed2bdcc5fcf3e9bc14d2b1055000748eaca502d
edanrenecreates <rene@edanramos.com>
2020-10-08 19:54:32 -0600

    Built fromImage componenent and place one on newsletter new form

--- src/components/newsletter/newsletterNewForm.js
+++ src/components/newsletter/newsletterNewForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { reduxForm, Field, submit } from 'redux-form';
 
 import { FormTitle } from '../formTitle';
-import { FormInput, FormButton, FormTextArea } from '../formFields';
+import { FormInput, FormButton, FormTextArea, FormImage } from '../formFields';
 import TextLink from '../textLink';
 
 class NewNewsletterForm extends Component {
@@ -43,6 +43,14 @@ class NewNewsletterForm extends Component {
           title="Cancel"
           component={FormButton}
           onClick={this.props.onCancel}
+        />
+        <Field className='new-newsletter-form__image'
+          small={true}
+          danger={false}
+          name='image'
+          type='file'
+          title="Image"
+          component={FormImage}
         />
         </form>
     )
