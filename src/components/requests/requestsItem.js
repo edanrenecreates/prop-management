07142988ccb578b654c4a02ab773354ef94cf376
edanrenecreates <rene@edanramos.com>
2020-10-20 20:51:15 -0600

    cleaned up request item grid.

--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -10,12 +10,13 @@ class RequestsItem extends Component {
       <div className='requests-item'>
         <Icon className='requests-item__icon' icon='fas fa-exclamation-triangle'/>
         <div className='requests-item__title'>
-          The bathroom sink is clogged
+          <div className='requests-item__title__text' >Yo my door fell down</div>
+          <Icon className='requests-item__title__arrow' icon='fas fa-sort-down' />
         </div>
         <div className='requests-item__tenant-unit'>
           Tom - Unit 220
         </div>
-        <Icon className='requests-item__arrow' icon='fas fa-sort-down'/>
+    
         <div className='requests-item__date'>
           10/16/1997
         </div>
