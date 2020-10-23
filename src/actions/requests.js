b345ed6d9d3b1b10c4ba3492b0640af85e92c4cc
edanrenecreates <rene@edanramos.com>
2020-10-22 19:41:05 -0600

    built create new request action creator with redux-thunk middleware

--- src/actions/requests.js
+++ src/actions/requests.js
@@ -2,6 +2,10 @@ import {
   CHANGE_SELECTED_REQUEST_TYPE
 } from './types';
 
+import axions from 'axios';
+import { ROOT_URL } from '../config';
+import Axios from 'axios';
+
 export function changeSelectedRequestType(boxType) {
 
   return (
@@ -11,3 +15,17 @@ export function changeSelectedRequestType(boxType) {
     }
   )
 };
+
+export function createNewRequest(newRequest, success) {
+  return function() {
+    Axios.post=(`${ROOT_URL}/requests/new`, newRequest)
+      then(response => {
+        console.log(response.data);
+        success();
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+}
