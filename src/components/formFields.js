ff2f3fb52f829640f350248aed4370ec041c4132
edanrenecreates <rene@edanramos.com>
2020-10-23 18:54:34 -0600

    formdata and input onchange

--- src/components/formFields.js
+++ src/components/formFields.js
@@ -64,8 +64,10 @@ export class FormButton extends Component {
 export class FormImage extends Component {
 
   handleSelectedImage = (event) => {
+    const { input: { onChange } } = this.props;
     let image = document.getElementById('newsletter-new-image');
     image.src = URL.createObjectURL(event.target.files[0]);
+    onChange(event.target.files[0])
   }
 
 
@@ -82,7 +84,6 @@ export class FormImage extends Component {
         <input
           className='form-image__replace'
           type='button'
-          //id=''
           value='Replace'
           onClick={
             () => 
--- src/components/requests/requestsNew.js
+++ src/components/requests/requestsNew.js
@@ -7,10 +7,14 @@ import NewNewsletterForm from "../newsletter/newsletterNewForm";
 
 class NewRequest extends Component {
   onSubmit = fields => {
-    // if(button == 'submit') {
-    //   // save new newsletter on the backend. perform a post request here.
-    //   console.log('trying to submit to backend.');
-    // }
+    const { title, body, image } = fields;
+    let formData = new FormData();
+    formData.append('title', title)
+    formData.append('body', body)
+    formData.append('image', image)
+
+    console.log(title, body, image);
+
     this.props.createNewRequest(this.props._id, fields, () => {
       this.props.history.push("/dashboard");
     })
